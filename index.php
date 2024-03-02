7ff9b3d5423f87cd072fa7a0614a0a2796a836cd
GabR3924 <garc2004@gmail.com>
2024-03-02 14:09:58 -0400

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    $nombre = "gabriela rodriguez";
+    $NOMBRE = "otro nombre";  
+
+    define('PRUEBA', 'averaverAAAAAA');
+
+    define('ANIMALES', [
+        'perro',
+        'gato',
+        'loro',
+        'serpiente',
+    ])
+
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1><?php echo ANIMALES[1]; ?></h1>
+    </body>
+    </html>
